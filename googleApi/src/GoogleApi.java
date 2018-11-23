83828001d48e24df60ae1b2111ec1da5f9daddd4
HikkaV <urumipainblackreaper@gmail.com>
2018-11-23 14:35:23 +0200

    first commit

---
+++ googleApi/src/GoogleApi.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoogleApi
+{
+    public static void main(String[] args) {
+        int mas []= new int [20];
+        Arrays.stream(mas).sorted().max();
+    }
+}
